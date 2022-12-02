745901960d8e08126cefc8bfb2e4f19cf8e6c170
Tester Oprogramowania <testeroprogramowaniakontakt@gmail.com>
2022-12-02 14:32:32 +0100

    add rest assured tests

---
+++ src/main/java/Comment.java
@@ -0,0 +1,39 @@
+public class Comment {
+
+    private String body;
+    private String username;
+    private String postId;
+    private String id;
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ src/test/java/FakeApiTests.java
@@ -0,0 +1,35 @@
+import static io.restassured.RestAssured.*;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class FakeApiTests {
+
+
+    @Test
+   public void getSpecificComment() {
+        Response response = get("http://localhost:3000/comments/1");
+
+        Comment specificComment = response.as(Comment.class);
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+        Assert.assertNotNull(specificComment);
+        Assert.assertEquals(specificComment.getId(),"1");
+    }
+
+    @Test
+    public void getAllComments() {
+        Response response = get("http://localhost:3000/comments");
+
+        JsonPath jsonPathEvaluator = response.jsonPath();
+        List<Comment> allComents = jsonPathEvaluator.getList(".", Comment.class);
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+        Assert.assertTrue(allComents.size()>1);
+    }
+
+}
